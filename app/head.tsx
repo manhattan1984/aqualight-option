aca9a97be0909773f75c51a745daaae8a3093d9e
manhattan1984 <103131213+manhattan1984@users.noreply.github.com>
2024-10-06 19:53:46 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -10,15 +10,16 @@ export default function Head() {
       />
       <meta name="description" content="Aqualight Option" />
       <link rel="icon" href="/favicon.ico" />
-      
+
       {/* Tawk.to Script */}
-      <div
+      <Script
+        id="tawk-to"
+        strategy="lazyOnload"
         dangerouslySetInnerHTML={{
           __html: `
             var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
             (function(){
-              var s1=document.createElement("script"),
-              s0=document.getElementsByTagName("script")[0];
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
               s1.async=true;
               s1.src='https://embed.tawk.to/6702c671256fb1049b1e2139/1i9hdeccu';
               s1.charset='UTF-8';
