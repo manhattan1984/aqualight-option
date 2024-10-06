b00174b5c480e561ed526e34181d64f92ac7fec8
manhattan1984 <103131213+manhattan1984@users.noreply.github.com>
2024-10-06 19:44:17 +0100

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -1,17 +1,33 @@
-import Script from "next/script";
+import Script from 'next/script';
 
 export default function Head() {
   return (
     <>
       <title>Aqualight Option</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        content="width=device-width, initial-scale=1"
+        name="viewport"
+      />
       <meta name="description" content="Aqualight Option" />
       <link rel="icon" href="/favicon.ico" />
-
-      <Script
-        src="//code.tidio.co/ecq2swrggafk3ukp9tgrlzpcxvlaaejn.js"
-        async
-      ></Script>
+      
+      {/* Tawk.to Script */}
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),
+              s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/6702c671256fb1049b1e2139/1i9hdeccu';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `,
+        }}
+      />
     </>
   );
 }
